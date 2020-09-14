fc7043f03603ad49e766d7f587d992dd1b87b4e0
cmtucker <carolinemtucker@gmail.com>
2020-09-14 16:24:56 -0700

    file structure, test script

---
+++ Rscripts/0_testPrioritizerPkg.R
@@ -0,0 +1,14 @@
+#####################################################################
+# a238 Test Prioritizer library  
+# 09-2020                                       					#
+#                                                                   #                                       #
+#                                                                   #
+# Script by C Tucker for ApexRMS 									#
+#####################################################################
+
+# Workspace ---------------------------------------------------------
+## Packages
+  #To initially install
+
+install.packages("prioritizr", repos = "https://cran.rstudio.com/")
+library(prioritizr)
