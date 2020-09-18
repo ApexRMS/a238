7da18981993ac05611923b5ccc23ec3052c93d46
cmtucker <carolinemtucker@gmail.com>
2020-09-17 21:09:15 -0700

    Updated data examples

--- Rscripts/0_testPrioritizerPkg.R
+++ Rscripts/0_testPrioritizerPkg.R
@@ -72,6 +72,8 @@ protectedAreas <- raster(file.path(outDir, "protectedAreasFocalArea.tif"))
 protectedAreasFocal <-  mask(protectedAreas, naturalAreasFocal)  
 
 
+## Generate input files for prioritizR
+
 ## Load files for all species using for loop
 specieslist <- c("BLBR", "MAAM", "URAM", "RANA", "PLCI")
 
@@ -82,16 +84,12 @@ species <- i		#BLBR  MAAM  URAM  RANA  PLCI
   # Load Focal species data 
   # Range scale features to have suitabilities 0-1
 habitatSuitability <- raster(file.path(outDir, paste0(species, "_HabitatSuitability_FocalArea.tif"))) %>%
-  					  calc(., fun = rescaleR)
+  				calc(., fun = rescaleR)
 				nam <- paste0(species, "_habitatSuitability")
 				assign(nam, habitatSuitability)
 
-habitatPatch <- raster(file.path(outDir, paste0(species, "_HabitatPatch_FocalArea.tif"))) %>%
-  				calc(., fun = rescaleR)
-				nam2 <- paste0(species, "_habitatPatch")
-				assign(nam2, habitatPatch)
-
 curmap <- raster(file.path(outDir, paste0(species, "_curmap_FocalArea.tif"))) %>%
+				log(.) %>% #log values
   				calc(., fun = rescaleR)
 				nam3 <- paste0(species, "_curmap")
 				assign(nam3, curmap)
@@ -102,50 +100,52 @@ habitatArea <- raster(file.path(outDir, paste0(species, "_HabitatArea_FocalArea.
 				assign(nam4, habitatArea)
 
 }
+#Ignore warnings
 
-#BLBR_habitatSuitability
-#MAAM_habitatSuitability
-#URAM_habitatSuitability
-#RANA_habitatSuitability
-#PLCI_habitatSuitability
 
-#stack types of suitabilities
-allHabitatSuitabilities <- stack(BLBR_habitatSuitability, MAAM_habitatSuitability, URAM_habitatSuitability, RANA_habitatSuitability, PLCI_habitatSuitability)
-allHabitatAreas <- stack(BLBR_habitatArea, MAAM_habitatArea, URAM_habitatArea, RANA_habitatArea, PLCI_habitatArea)
-allHabitatPatch <- stack(BLBR_habitatPatch, MAAM_habitatPatch, URAM_habitatPatch, RANA_habitatPatch, PLCI_habitatPatch)
-allCurMap <- stack(BLBR_curmap, MAAM_curmap, URAM_curmap, RANA_curmap, PLCI_curmap)
-
-#combine for all suitabilities
+  # Combine for all features
 allSuitabilities <- stack(BLBR_habitatSuitability, MAAM_habitatSuitability, URAM_habitatSuitability, RANA_habitatSuitability, PLCI_habitatSuitability, BLBR_habitatArea, MAAM_habitatArea, URAM_habitatArea, RANA_habitatArea, PLCI_habitatArea, BLBR_curmap, MAAM_curmap, URAM_curmap, RANA_curmap, PLCI_curmap)
   spplot(allSuitabilities)
 
-
-## Generate input files for prioritizR
-
-  # x here is focal area raster with protected areas coded in locked in/locked out attribute data
- # single species example 
+## Run prioritizr
+ # Single species example 
+ # Default, fast conditions
+ 
 test <- problem(protectedAreasFocal, features = BLBR_habitatSuitability) %>%
 			add_min_set_objective() %>%
-      		add_relative_targets(0.2) %>%
+      		add_relative_targets(0.05) %>%
       		add_binary_decisions() %>%
       		add_default_solver(gap = 0)
      		
 P1 <- solve(test)
-
-par(mfrow=c(1,3))
-plot(BLBR_habitatSuitability)
-plot(protectedAreasFocal)
+ #  user  system elapsed 
+ #  1.445   0.401   2.049 
 spplot(P1)
 
+  # To incorporate existing protected areas as constraint (i.e. they must be locked in to the final example)
+test2 <- problem(protectedAreasFocal, features = BLBR_habitatSuitability) %>%
+			add_min_set_objective() %>%
+      		add_relative_targets(0.05) %>%
+      		add_binary_decisions() %>%
+      		add_default_solver(gap = 0) %>%
+      		add_locked_in_constraints(protectedAreasFocal)
+P2 <- solve(test2)
+
+
 # Multiple species problem
-#for all natural areas in Monteregie
-# this includes constraints that 'lock_in' protected areas
 testMulti <- problem(naturalAreasBinaryFocal, features = allSuitabilities) %>%
+			    add_min_set_objective() %>%
+      			add_relative_targets(0.05) %>%
+      			add_binary_decisions() %>%
+      			add_default_solver(gap = 0)
+     			
+PMulti<- solve(testMulti)
+spplot(PMulti)
+
+# this includes constraints that 'lock_in' protected areas
+testMulti2 <- problem(naturalAreasBinaryFocal, features = allSuitabilities) %>%
 			    add_min_set_objective() %>%
       			add_relative_targets(0.2) %>%
       			add_binary_decisions() %>%
       			add_default_solver(gap = 0)  %>%
      			add_locked_in_constraints(protectedAreasFocal)
-     			
-PMulti<- solve(testMulti)
-spplot(PMulti)
