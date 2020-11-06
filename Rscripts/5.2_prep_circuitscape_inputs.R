3eb2e99cd140bc1c2ebb07a5821646efe76f0645
vlucet <VLucet@users.noreply.github.com>
2020-11-06 23:29:46 +0000

    get all INI right

--- Rscripts/5.2_prep_circuitscape_inputs.R
+++ Rscripts/5.2_prep_circuitscape_inputs.R
@@ -8,35 +8,27 @@
 library(stringr)
 library(raster)
 
-print("Preparing circuitscape inputs"); Sys.time()
-
 ## Read in 
-reclassed_list <- list.files("Data/Processed/", 
+reclassed_list <- list.files("Data/Processed/",
+                             full.names = TRUE,  
                              pattern = "Resistance")
 
-# Make new files list 
-buffered_list <-paste0(reclassed_list, "with_buffer")
-
 # Make all the INI files
-INI_template <- readLines("config/ini_circuitscape/template/ini_template.ini")
+INI_template <- readLines("config/ini_template.ini")
 
-make_INI_file = function(file, orientation) {
+make_INI_file = function(file) {
+  base <- tools::file_path_sans_ext(basename(file))
   INI <- lapply(X=INI_template, FUN=str_replace_all, 
-                pattern ="RESISTANCE_FILE_NON_ORIENTED",
-                replacement = paste0("outputs/reclassed_with_buffer/",file,".tif"))
-  
-  INI <- lapply(X=INI, FUN=str_replace_all, 
-                pattern ="POINT_FILE_ORIENTED",
-                replacement = paste0("outputs/reclassed_with_buffer/circuitscape_buffer_", 
-                                     orientation, ".tif"))
+                pattern ="RESISTANCE_FILE",
+                replacement = file)
   INI <- lapply(X=INI, FUN=str_replace_all, 
                 pattern ="OUTPUT_FILE",
-                replacement = paste0("outputs/current_density/",file, "_", orientation, 
-                                     "_out")) # Np need for .tif here
-  fileConn<-file(paste0("config/ini_circuitscape/all/", file, "_",orientation,".ini"))
+                replacement = paste0("outputs/current_density/",
+                                     base, 
+                                     "_out")) # No need for .tif here
+  fileConn<-file(paste0("config/all/", base, ".ini"))
   writeLines(unlist(INI), fileConn)
   close(fileConn)
 }
 
-saved_output_EW <- lapply(buffered_list, make_INI_file, orientation="EW")
-saved_output_NS <- lapply(buffered_list, make_INI_file, orientation="NS")
+saved_output <- lapply(reclassed_list, make_INI_file)
