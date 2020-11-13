ea1031fedb015b83309f6d81037ba7db530b6d49
vlucet <VLucet@users.noreply.github.com>
2020-11-13 21:52:15 +0000

    crop initial conditions

--- Rscripts/7_prepare_stsim_inputs.R
+++ Rscripts/7_prepare_stsim_inputs.R
@@ -4,6 +4,14 @@ library(sf)
 
 primaryStratum <- 
   raster("libraries/BTSL_stconnect.ssim/BTSL_stconnect.ssim.input/Scenario-127/stsim_InitialConditionsSpatial/PrimaryStratum_NAchanged.tif")
+secondaryStratum <- 
+  raster("libraries/BTSL_stconnect.ssim/BTSL_stconnect.ssim.input/Scenario-127/stsim_InitialConditionsSpatial/SecondaryStratum_NAchanged.tif")
+tertiaryStratum <- 
+  raster("libraries/BTSL_stconnect.ssim/BTSL_stconnect.ssim.input/Scenario-127/stsim_InitialConditionsSpatial/TertiaryStratum_NAchanged.tif")
+stateclasses <- 
+  raster("libraries/BTSL_stconnect.ssim/BTSL_stconnect.ssim.input/Scenario-127/stsim_InitialConditionsSpatial/InitialStateClass_AgeMean_NAchanged.tif")
+
+
 studyExtent <- raster("Data/Processed/LULC_FocalAreaBuffer.tif")
 onlyMonteregie <- st_transform(st_read("Data/Processed/regioMonteregie.shp"),
                                crs(studyExtent))
@@ -12,6 +20,23 @@ primaryStratumCropped <-
   extend(mask(crop(primaryStratum, studyExtent), onlyMonteregie), studyExtent)
 primaryStratumCropped[is.na(primaryStratumCropped)] <- 0
 
+secondaryStratumCropped<- 
+  extend(mask(crop(secondaryStratum, studyExtent), onlyMonteregie), studyExtent)
+tertiaryStratumCropped<- 
+  extend(mask(crop(tertiaryStratum, studyExtent), onlyMonteregie), studyExtent)
+stateclassesCropped<- 
+  extend(mask(crop(stateclasses, studyExtent), onlyMonteregie), studyExtent)
+
+
 writeRaster(primaryStratumCropped, 
             "Data/stsim/primary_stratum_FocalAreaBuffer.tif",
+            overwrite = TRUE)
+writeRaster(secondaryStratumCropped, 
+            "Data/stsim/secondary_stratum_FocalAreaBuffer.tif",
+            overwrite = TRUE)
+writeRaster(tertiaryStratumCropped, 
+            "Data/stsim/tertiary_stratum_FocalAreaBuffer.tif",
+            overwrite = TRUE)
+writeRaster(stateclassesCropped, 
+            "Data/stsim/stateclasses_FocalAreaBuffer.tif",
             overwrite = TRUE)
